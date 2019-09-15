6fb8f516e86039538c59ab9fe3685f1a902465d0
katrin <katrin.kampfrath@gmail.com>
2019-09-15 17:08:12 +0200

    Initial setup based on electron-vue + updates to electron6, babel7.

---
+++ src/main/index.dev.js
@@ -0,0 +1,24 @@
+/**
+ * This file is used specifically and only for development. It installs
+ * `electron-debug` & `vue-devtools`. There shouldn't be any need to
+ *  modify this file, but it can be used to extend your development
+ *  environment.
+ */
+
+/* eslint-disable */
+const { default: installExtension, VUEJS_DEVTOOLS } = require('electron-devtools-installer');
+
+// Install `electron-debug` with `devtron`
+require('electron-debug')({ showDevTools: true })
+
+// Install `vue-devtools`
+require('electron').app.on('ready', () => {
+  installExtension(VUEJS_DEVTOOLS)
+    .then(() => {})
+    .catch(err => {
+      console.log('Unable to install `vue-devtools`: \n', err)
+    })
+})
+
+// Require `main` process to boot app
+require('./index')
