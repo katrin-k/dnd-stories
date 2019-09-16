63a99b1eae1d969b6d3322ff566b24ca5bd9eebc
katrin <katrin.kampfrath@gmail.com>
2019-09-16 11:06:44 +0200

    Fix conflict between eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: 'babel-eslint',
-    sourceType: 'module'
+    parser: "babel-eslint",
+    sourceType: "module"
   },
   env: {
     browser: true,
@@ -11,21 +11,20 @@ module.exports = {
   extends: [
     "standard",
     "plugin:vue/recommended",
-    "prettier/vue",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
+    "prettier",
+    "prettier/vue"
   ],
   globals: {
     __static: true
   },
-  plugins: [
-    'vue'
-  ],
-  'rules': {
+  plugins: ["vue"],
+  rules: {
     // allow paren-less arrow functions
-    'arrow-parens': 0,
+    "arrow-parens": 0,
     // allow async-await
-    'generator-star-spacing': 0,
+    "generator-star-spacing": 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0
   }
-}
+};
